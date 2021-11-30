f9333e74a7ee7c6895f0bc56302880b4d8ff2d48
Thomas Sansebastien <thomas.sansebastien@etud.univ-pau.fr>
2021-11-30 17:33:41 +0100

    Ajout de route Entreprises/Formations/Stages par id

--- src/Controller/MetierController.php
+++ src/Controller/MetierController.php
@@ -9,12 +9,41 @@ use Symfony\Component\Routing\Annotation\Route;
 class MetierController extends AbstractController
 {
     /**
-     * @Route("/metier", name="metier")
+     * @Route("/", name="metier_accueil")
      */
     public function index(): Response
     {
         return $this->render('metier/index.html.twig', [
             'controller_name' => 'MetierController',
         ]);
+    }
+      /**
+     * @Route("/entreprises", name="metier_entreprises")
+     */
+    public function entreprises(): Response
+    {
+        return $this->render('metier/entreprises.html.twig', [
+            'controller_name' => 'MetierController',
+        ]);
+    }
+      /**
+     * @Route("/formation", name="metier_formation")
+     */
+    public function formation(): Response
+    {
+        return $this->render('metier/formation.html.twig', [
+            'controller_name' => 'MetierController',
+        ]);
+    }
+      /**
+     * @Route("/stages/{id}", name="metier_stages_id")
+     */
+    public function stages($id): Response
+    {
+        return $this->render('metier/stages.html.twig', [
+            'controller_name' => 'MetierController',
+            'id'=>$id,
+        ]);
+
     }
 }
