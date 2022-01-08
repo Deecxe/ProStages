2ebf2110fc0c6b7c9594ad6c7014c96361a0fb7f
Thomas Sansebastien <thomas.sansebastien@etud.univ-pau.fr>
2022-01-08 15:07:01 +0100

    Ajout des données des autres tables (Probleme)

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,6 +5,8 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Entreprise;
+use App\Entity\Formation;
+use App\Entity\Stage;
 
 class AppFixtures extends Fixture
 {
@@ -18,11 +20,38 @@ class AppFixtures extends Fixture
             $Entreprise = new Entreprise();
             $Entreprise->setNom($faker->company);
             $Entreprise->setAdresse($faker->streetAddress);
-            $Entreprise->setActivite($faker->realText($maxNbChars = 200, $indexSize = 2));
+            $Entreprise->setActivite($faker->realText($maxNbChars = 50, $indexSize = 2));
             $Entreprise->setSiteweb($faker->domainName);
             $manager->persist($Entreprise); 
         }
         
+        $nbFormation = 4;
+        for($i = 1; $i <= $nbFormation; $i++)
+        {
+            $Formation = new Formation();
+            $Formation->setNomCourt($faker->realText($maxNbChars = 10, $indexSize = 2));
+            $Formation->setNomLong($faker->realText($maxNbChars = 100, $indexSize = 2));
+            $manager->persist($Formation); 
+        }
+
+        $nbFormation = 6;
+        for($i = 1; $i <= $nbFormation; $i++)
+        {
+            $Stages = new Stage();
+            $Stages->setTitre($faker->realText($maxNbChars = 10, $indexSize = 2));
+            $Stages->setMission($faker->realText($maxNbChars = 200, $indexSize = 2));
+            $Stages->setEmail($faker->email);
+
+            $Stages->setTypeEntreprise($Entreprise);
+
+            $Stages->addTypeFormation($Formation);
+
+            $Entreprise->addStage($Stages);
+
+            $Formation->addStage($Stages);
+
+            $manager->persist($Stages); 
+        }
         $manager->flush();
     }
 }
